e3752163b1b66f4c8ec40d5568f0bc1cd4346c38
Emilia Zuñiga Losada <82168337+Emilia-Zuniga@users.noreply.github.com>
2023-04-19 17:22:01 -0600

    Add files via upload

---
+++ MatzuApp/Exercise1_5View.swift
@@ -0,0 +1,362 @@
+
+import SwiftUI
+
+struct Exercise1_5View: View {
+    
+    @State var Digit1_1: String = ""
+    @State var Digit1_2: String = ""
+    @State var Digit1_3: String = ""
+    @State var Digit1_4: String = ""
+    @State var Digit1_5: String = ""
+    @State var Digit1_6: String = ""
+    @State var Digit1_7: String = ""
+    @State var Digit1_8: String = ""
+    
+    @State var isPresented: Bool = false
+    
+    var body: some View {
+        ZStack{
+            Color.cyan
+            Image("Fondo")
+                .resizable(resizingMode:.tile)
+            
+            VStack{
+                Text("FIRST DEGREE EQUATIONS")
+                    .font(.custom("Impact", fixedSize: 40))
+                    .foregroundColor(.yellow)
+                    .shadow(color: Color.black,
+                            radius: 1.0,
+                            x: CGFloat(-7),
+                            y: CGFloat(1))
+                
+                Text("Exercise: 7X - 4 = 24")
+                    .foregroundColor(.black)
+                    .frame(width: 1100, height:50)
+                    .font(.custom("Helvetica", fixedSize: 30))
+                    .background(Color.Azulcielo)
+                    .opacity(0.67)
+                    .cornerRadius(30.0)
+                
+                ZStack{
+                    Color.white
+                        .frame(width: 1100, height:530)
+                        .cornerRadius(50.0)
+                        .clipShape(RoundedRectangle(cornerSize: .zero))
+                        .opacity(0.67)
+                        .overlay(
+                            RoundedRectangle(cornerSize: .zero)
+                                .stroke(LinearGradient(gradient: Gradient(colors: [Color.Oro, Color.yellow]), startPoint: .leading, endPoint: .trailing), lineWidth: 40)
+                                .cornerRadius(30.0)
+                        )
+                    
+                    VStack{
+                        HStack{
+                            Text("7")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                            
+                            Text("X")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                            
+                            Image(systemName: "minus")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            Text("4")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                            
+                            Image(systemName: "equal")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            Text("24")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                        }
+                        .padding(.leading, -300)
+                        .padding(10)
+                        
+                        HStack{
+                            TextField("", text: $Digit1_1)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                            
+                                .onChange(of: Digit1_1, perform: { value in
+                                    
+                                })
+                            
+                            Text("X")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                            
+                            Image(systemName: "equal")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            TextField("", text: $Digit1_2)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                            
+                                .onChange(of: Digit1_2, perform: { value in
+                                    
+                                })
+                            
+                            Image(systemName: "plus")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            TextField("", text: $Digit1_3)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 10)
+                            
+                                .onChange(of: Digit1_3, perform: { value in
+                                    
+                                })
+                            
+                        }
+                        .padding(.leading, 121)
+                        //.padding([.bottom, .trailing], 25)
+                        
+                        HStack{
+                            TextField("", text: $Digit1_4)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                            
+                                .onChange(of: Digit1_4, perform: { value in
+                                    
+                                })
+                            
+                            Text("X")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                            
+                            Image(systemName: "equal")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            TextField("", text: $Digit1_5)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 10)
+                            
+                                .onChange(of: Digit1_5, perform: { value in
+                                    
+                                })
+                        }
+                        .padding(10)
+                        .padding(.leading, -100)
+                        
+                        HStack{
+                            Text("X")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                            
+                            Image(systemName: "equal")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            TextField("", text: $Digit1_6)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 10)
+                            
+                                .onChange(of: Digit1_6, perform: { value in
+                                    
+                                })
+                            
+                            Image(systemName: "divide")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            TextField("", text: $Digit1_7)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 10)
+                            
+                                .onChange(of: Digit1_7, perform: { value in
+                                    
+                                })
+                        }
+                        .padding(.leading, 187)
+                        
+                        HStack{
+                            Text("X")
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 30)
+                            
+                            Image(systemName: "equal")
+                                .font(.custom("Arial Black", size: 60))
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 30)
+                            
+                            TextField("", text: $Digit1_8)
+                                .keyboardType(.numberPad)
+                                .padding(30)
+                                .foregroundColor(.black)
+                                .frame(width: 83, height:80)
+                                .font(.custom("Helvetica", fixedSize: 30))
+                                .background(Color.Verdelima)
+                                .cornerRadius(50.0)
+                                .shadow(radius: 10)
+                                .padding(.trailing, 10)
+                            
+                                .onChange(of: Digit1_8, perform: { value in
+                                    
+                                })
+                        }
+                        .padding(20)
+                        .padding(.leading, -10)
+                    }
+                }
+                
+                //Investigar si se puede agregar una condicion por el resultado
+                //en gaming plus minus
+                // en all equal
+                
+                if Digit1_1 == "" || Digit1_2 == "" || Digit1_3 == "" || Digit1_4 == "" || Digit1_5 == "" || Digit1_6 == "" || Digit1_7 == "" || Digit1_8 == "" {
+                    Button("I have finished") {
+                        isPresented = true
+                    }
+                    .foregroundColor(.black)
+                    .frame(width: 200, height:50)
+                    .font(.custom("Helvetica", fixedSize: 30))
+                    .background(Color.Verdelima)
+                    .cornerRadius(30.0)
+                    .shadow(radius: 10)
+                    .alert(isPresented: $isPresented, content: {
+                        Alert(title: Text("Empty fields"),
+                              message: Text("It is necessary to complete all the fields"),
+                              
+                              primaryButton: Alert.Button.default(Text("Accept"), action: {
+                            
+                        }),
+                              secondaryButton: .destructive(Text("Cancel")))
+                    })
+                    
+                } else if Digit1_8 == "4" {
+                    NavigationLink(destination: ExerciseF1()){
+                        Text("I have finished")
+                            .foregroundColor(.black)
+                            .frame(width: 200, height:50)
+                            .font(.custom("Helvetica", fixedSize: 30))
+                            .background(Color.Verdelima)
+                            .cornerRadius(30.0)
+                            .shadow(radius: 10)
+                    }
+                }else {
+                    NavigationLink(destination: Error1View()){
+                        Text("I have finished")
+                            .foregroundColor(.black)
+                            .frame(width: 200, height:50)
+                            .font(.custom("Helvetica", fixedSize: 30))
+                            .cornerRadius(30.0)
+                            .shadow(radius: 10)
+                    }
+                }
+            }
+        }.ignoresSafeArea()
+        
+    }
+}
+
+
+struct Exercise1_5View_Previews: PreviewProvider {
+    static var previews: some View {
+        Exercise1_5View()
+    }
+}
+
+
+
---
+++ MatzuApp/Levels1View.swift
@@ -0,0 +1,134 @@
+//
+//  NivelesView.swift
+//  Matzu
+//
+//  Created by aleyda contreras natareno on 06/04/23.
+//
+
+import SwiftUI
+
+struct Levels1View: View {
+    var body: some View {
+        ZStack(){
+            Color.cyan
+            Image("Fondo")
+                .resizable(resizingMode:.tile)
+            
+            VStack{
+                Spacer()
+                Text("FIRST DEGREE EQUATIONS")
+                    .font(.custom("Impact", fixedSize: 40))
+                    .foregroundColor(.yellow)
+                    .shadow(color: Color.black,
+                            radius: 1.0,
+                            x: CGFloat(-7),
+                            y: CGFloat(1))
+                
+                Text("Select a level and have fun")
+                    .foregroundColor(.black)
+                    .frame(width: 1100, height:50)
+                    .font(.custom("Helvetica", fixedSize: 30))
+                    .background(Color.Azulcielo)
+                    .opacity(0.67)
+                    .cornerRadius(30.0)
+                
+                NavigationLink(destination: Example1View()){
+                    Text("Example")
+                        .foregroundColor(.black)
+                        .frame(width: 200, height:50)
+                        .font(.custom("Helvetica", fixedSize: 30))
+                        .background(Color.Verdelima)
+                        .cornerRadius(30.0)
+                        .shadow(radius: 10)
+                }
+                
+                HStack{
+                    Spacer()
+                    NavigationLink(destination: Exercise1_1View()){
+                        Text("1")
+                            .foregroundColor(.black)
+                            .frame(width: 83, height:80)
+                            .font(.custom("Helvetica", fixedSize: 30))
+                            .background(Color.Verdelima)
+                            .cornerRadius(50.0)
+                            .shadow(radius: 10)
+                            .padding(.trailing, 38)
+                    }
+                    Spacer()
+                    NavigationLink(destination: Exercise1_3View()){
+                        Text("3")
+                            .foregroundColor(.black)
+                            .frame(width: 83, height:80)
+                            .font(.custom("Helvetica", fixedSize: 30))
+                            .background(Color.Verdelima)
+                            .cornerRadius(50.0)
+                            .shadow(radius: 10)
+                            .padding(.trailing, 0)
+                    }
+                    Spacer()
+                    NavigationLink(destination: Exercise1_5View()){
+                        Text("5")
+                            .foregroundColor(.black)
+                            .frame(width: 83, height:80)
+                            .font(.custom("Helvetica", fixedSize: 30))
+                            .background(Color.Verdelima)
+                            .cornerRadius(50.0)
+                            .shadow(radius: 10)
+                            .padding(.leading, 40)
+                    }
+                    Spacer()
+                }
+                
+                Image("Level1")
+                    .resizable()
+                    .frame(width: 900, height: 250)
+                    .cornerRadius(30.0)
+                
+                HStack{
+                    Spacer()
+                    NavigationLink(destination: Exercise1_2View()){
+                        Text("2")
+                            .foregroundColor(.black)
+                            .frame(width: 83, height:80)
+                            .font(.custom("Helvetica", fixedSize: 30))
+                            .background(Color.Verdelima)
+                            .cornerRadius(50.0)
+                            .shadow(radius: 10)
+                            .padding(.trailing, 38)
+                    }
+                    .padding(.trailing, 210)
+                    NavigationLink(destination: Exercise1_4View()){
+                        Text("4")
+                            .foregroundColor(.black)
+                            .frame(width: 83, height:80)
+                            .font(.custom("Helvetica", fixedSize: 30))
+                            .background(Color.Verdelima)
+                            .cornerRadius(50.0)
+                            .shadow(radius: 10)
+                            .padding(.trailing, 375)
+                    }
+                    
+                }
+                Spacer()
+                NavigationLink(destination: LearnMore1View()){
+                    Text("Learn more")
+                        .foregroundColor(.black)
+                        .frame(width: 200, height:50)
+                        .font(.custom("Helvetica", fixedSize: 30))
+                        .background(Color.Verdelima)
+                        .cornerRadius(30.0)
+                        .shadow(radius: 10)
+                }
+                Spacer()
+            }
+            
+        }.ignoresSafeArea()
+    }
+    
+}
+
+struct Levels1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Levels1View()
+    }
+}
